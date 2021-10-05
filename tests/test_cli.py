7e28061f88cebc4d78cdc983d38e72d49bf0104f
nialov <nikolasovaskainen@hotmail.com>
2021-10-05 14:01:46 +0300

    test(cli): test overwrite flag

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,11 +9,13 @@ from kapalo_py import cli
 
 @pytest.mark.parametrize("origin_dir,extension", tests.test__resize_images_params())
 @pytest.mark.parametrize("fixed_width", [400, 800])
-def test__resize_images(origin_dir, extension, fixed_width, tmp_path):
+@pytest.mark.parametrize("overwrite", [True, False])
+def test__resize_images(origin_dir, extension, fixed_width, tmp_path, overwrite):
     """
     Test _resize_images.
     """
     assert origin_dir.exists()
+    assert isinstance(overwrite, bool)
     origin_dir_files = list(origin_dir.glob(f"*.{extension}"))
     origin_dir_filecount = len(origin_dir_files)
     assert origin_dir_filecount > 0
@@ -23,6 +25,7 @@ def test__resize_images(origin_dir, extension, fixed_width, tmp_path):
         destination_dir=destination_dir,
         extension=extension,
         fixed_width=fixed_width,
+        overwrite=overwrite,
     )
 
     destination_dir_files = list(destination_dir.glob(f"*.{extension}"))
